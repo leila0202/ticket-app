8cedf86d105a9a8d776aba20a21957ff88285bc0
Leila Zrim <leilazrim@gmail.com>
2023-11-15 14:53:48 +0100

    added styling for Progress Display and Status Display

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -15,8 +15,16 @@ const TicketCard = () => {
       <h4>Ticket Title</h4>
       <hr className="h-px border-0 bg-page mb-2" />
       <p className="whitespace-pre-wrap">this is the ticket description</p>
-      <ProgressDisplay />
-      <StatusDisplay />
+      <div className="flex-grow"></div>
+      <div className="flex mt-2">
+        <div className="flex flex-col">
+          <p className="text-xs my-1">15/11/2023 14:52</p>
+          <ProgressDisplay />
+        </div>
+        <div className="ml-auto flex items-end">
+          <StatusDisplay />
+        </div>
+      </div>
     </div>
   );
 };
