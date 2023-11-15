c57376aee674e2970adb5d6dc1f809109d186a46
Leila Zrim <leilazrim@gmail.com>
2023-11-15 15:34:00 +0100

    added Ticket Form with state

---
+++ app/(components)/TicketForm.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const TicketForm = () => {
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const startingTicketData = {
+    title: "",
+    description: "",
+    priority: 1,
+    progress: 0,
+    status: "not started",
+    category: "Hardware Problem",
+  };
+
+  const [formData, setFormData] = iseState(startingTicketData);
+
+  return (
+    <div className="flex justify-center">
+      <form>
+        <h3>Create a new Ticket:</h3>
+        <label>Title</label>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.title}
+        />
+      </form>
+    </div>
+  );
+};
+export default TicketForm;
