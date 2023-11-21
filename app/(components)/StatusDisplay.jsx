64d894a3db349ffd803d7334ab900e4a74f515d6
Leila Zrim <leilazrim@gmail.com>
2023-11-21 11:47:43 +0100

    use better color scheme in status display

--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,6 +1,24 @@
 const StatusDisplay = ({ status }) => {
+  let statusColor = "";
+  console.log(status);
+  switch (status) {
+    case "done":
+      statusColor = "bg-green-200";
+      break;
+    case "started":
+      statusColor = "bg-yellow-400";
+      break;
+    case "not started":
+      statusColor = "bg-red-200";
+      break;
+    default:
+      statusColor = "bg-slate-700";
+      break;
+  }
   return (
-    <span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200">
+    <span
+      className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${statusColor}`}
+    >
       {status}
     </span>
   );
