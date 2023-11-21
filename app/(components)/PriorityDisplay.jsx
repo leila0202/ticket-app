a8c34e6f15f08908055b25fd7263958e63a70325
Leila Zrim <leilazrim@gmail.com>
2023-11-21 11:43:24 +0100

    better priority indication styles following the tutorial

--- app/(components)/PriorityDisplay.jsx
+++ app/(components)/PriorityDisplay.jsx
@@ -2,12 +2,29 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFire } from "@fortawesome/free-solid-svg-icons";
 
 const PriorityDisplay = ({ priority }) => {
-  const priorities = [];
-  for (let index = 0; index < priority; index++) {
-    priorities[index] = (
-      <FontAwesomeIcon key={index} icon={faFire} className="text-red-400" />
-    );
-  }
-  return <div className="flex justify-start align-baseline">{priorities}</div>;
+  return (
+    <div className="flex justify-start align-baseline">
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 0 ? "text-red-400" : "text-slate-400"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 1 ? "text-red-400" : "text-slate-400"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 2 ? "text-red-400" : "text-slate-400"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 3 ? "text-red-400" : "text-slate-400"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 4 ? "text-red-400" : "text-slate-400"}`}
+      />
+    </div>
+  );
 };
 export default PriorityDisplay;
